b3e7bc2fa2f517433bd73dc05d09edc31f3deea4
Maxim Pokrovskii <maxim.pokrovskii@gmail.com>
2015-03-20 13:56:12 +0700

    init

---
+++ sampleAPI.js
@@ -0,0 +1,25 @@
+{
+    "apps": [{
+        "title": "Lazada – Shopping & Deals",
+        "desc": "Shop on Southeast Asia’s largest online shopping mall!",
+        "urlImg": "https://appnext-a.akamaihd.net//banner/K6dYU8nMsnE4FVk_default.png",
+        "urlApp": "https://admin.appnext.com/appLink.aspx?b=21418&e=34235&q=",
+        "revenueType": "cpi",
+        "revenueRate": "0.46",
+        "categories": "Catalogs,Lifestyle,Utilities",
+        "idx": "1",
+        "iosPackage": "785385147",
+        "supportedDevices": "iphone,ipad,ipod"
+    }, {
+        "title": "Dot Arena",
+        "desc": "เอเชีย # 1 เกมมือถือคือการเอเชียตะวันออกเฉียงใต้โดยพายุ!อิสระในการเล่น!",
+        "urlImg": "https://appnext-a.akamaihd.net//banner/SOzBpZS3L6sZSNT_uploaded.png",
+        "urlApp": "https://admin.appnext.com/appLink.aspx?b=19166&e=34235&q=",
+        "revenueType": "cpi",
+        "revenueRate": "0.78",
+        "categories": "Action,Card,Role Playing",
+        "idx": "1",
+        "iosPackage": "896830109",
+        "supportedDevices": "iphone,ipad,ipod"
+    }]
+}
