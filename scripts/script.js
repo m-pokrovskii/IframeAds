4385f6a9c8d07cc2110e8240355d2549a66a627a
Maxim Pokrovskii <maxim.pokrovskii@gmail.com>
2015-03-24 13:39:58 +0700

    aded a lot of functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,167 @@
-console.log('ready');
+// TODO
+
+// FONT change
+// - Refractoring
+// - Build Class new URI
+
+var APP = (function(){
+
+	var elements = {
+		app:   qs('.app'),
+		items: qs('.app__items'),
+		ad:    qs('.app__item')
+	};
+
+	var pu        = parseURL(),
+			q         = pu.query;
+	var url = getURL();
+
+	function init () {
+		var data = loadJSONP(url, renderADS);
+	}
+
+	function getURL () {
+		var mobileOS = getMobileOperatingSystem(),
+				id,
+				count = returnCountOfAd();
+				cat = q.cat || "";
+				pbk = q.pbk || "";
+
+		if (mobileOS === "iOS") {
+			id = q.ios_id;
+		} else if (mobileOS = 'Android') {
+			id = q.android_id;
+		} else {
+			id = 'c75c15f7-6e39-4e0a-b253-99157ed19cec';
+		};
+		staticURL = 'https://admin.appnext.com/offerWallApi.aspx?id='+id+'&cnt='+count+'&cat='+cat+'&pbk='+pbk+'&type=json';
+		return staticURL;
+	}
+
+function getMobileOperatingSystem() {
+		var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+		if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPod/i))
+		{
+				return 'iOS';
+		}
+		else if (userAgent.match(/Android/i))
+		{
+				return 'Android';
+		}
+	}
+
+	function parseURL () {
+		var queryParams = {};
+		var url         = window.location.href;
+		var parser      = document.createElement('a');
+		parser.href     = url;
+		
+		var splitURL = url.split("?");
+
+		var query = parser.search.substr(1).split("&");
+		query.forEach(function(el, i, arr) {
+			var values = arr[i].split("=");
+			queryParams[values[0]] = values[1];
+		});
+		return {
+			parser:       parser,
+			beforeSearch: splitURL[0],
+			query:        queryParams,
+		}
+	};
+
+	function renderADS (data) {
+		var countOfAds = returnCountOfAd();
+		if (countOfAds > 1) {
+			for (var i = countOfAds; i > 1; i--) {
+				var cln = elements.ad.cloneNode(true);
+				elements.items.appendChild(cln);
+			};
+		};
+		replaceContent(data);
+	}
+
+
+	function addElements () {
+			elements.ads    = qsa('.app__item');
+			elements.titles = qsa('.title');
+			elements.descs  = qsa('.description');
+			elements.images = qsa('.image-container img');
+			elements.buttons = qsa('.button > span');
+	}
+
+	function returnCountOfAd () {
+		var countOfAds = {
+			1: [ [216, 36], [300,50], [320, 50], [336, 280], [480, 320] ],
+			2: [ [120,600], [160,600], [320, 480], [468,60], [728,90] ],
+			3: [ [1024,90] ],
+		}
+
+		for (key in countOfAds) {
+			var arr = countOfAds[key];
+			for (var i = arr.length - 1; i >= 0; i--) {
+				if (arr[i].toString() == iframeSize().toString()) {
+					return key;
+				};
+			};
+		};
+	}
+
+	function iframeSize () {
+		return [window.frameElement.getAttribute('width'), window.frameElement.getAttribute('height')];
+	}
+
+	function replaceContent (data) {
+		var btn_text = q.btn_text,
+				btn_color = (q.btn_color) ? '#'+q.btn_color : "";
+		addElements();
+		data['apps'].forEach(function(el, i) {			
+			elements.titles[i].textContent = el.title;
+			elements.descs[i].textContent  = el.desc;
+			elements.images[i].setAttribute('src', el.urlImg);
+			elements.ads[i].addEventListener('click', function() {
+				window.top.location.href = el.urlApp;
+			})
+			
+			if (btn_text) {
+				elements.buttons[i].textContent  = q.btn_text;	
+			};
+
+			if (btn_color) {
+				elements.buttons[i].parentNode.style.backgroundColor = btn_color;
+			};
+
+			elements.app.style.display = 'block';
+		});
+	}
+
+	function loadJSONP (url, callback, context) {
+		var unique = 0;
+		var name = "_jsonp_" + unique++;
+
+		if (url.match(/\?/)) url += "&callback="+name;
+		else url += "?callback="+name;
+
+		// Create script
+		var script  = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src  = url;
+
+		// Setup handler
+		window[name] = function(data){
+			callback.call((context || window), data);
+			document.getElementsByTagName('head')[0].removeChild(script);
+			script = null;
+			delete window[name];
+		};
+
+		// Load JSON
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
+
+	return {
+		init: init
+	}	
+}());
+
+APP.init();
